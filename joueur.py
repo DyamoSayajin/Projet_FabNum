3135de5817697e29c63bdf14bece3e402a59a91e
Hichem <148470835+Hichem212@users.noreply.github.com>
2024-03-25 20:40:47 +0100

    creation de la class joueur

---
+++ joueur.py
@@ -0,0 +1,19 @@
+class Joueur:
+    def __init__(self, nom, score):
+        self.nom = nom
+        self.score = score
+
+    def ajouterScore(self, score):
+        self.score += score
+    
+    def donner_score(self):
+        return self.score
+    
+    
+
+
+
+
+# Création des joueurs
+joueur1 = Joueur("Equipe 1", 0)
+joueur2 = Joueur("Equipe 2", 0)
